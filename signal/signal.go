5cd38acf733e5b6cf8ed01e89f12c55b680bb061
DNT <ducconit@gmail.com>
2022-12-07 17:39:29 +0700

    add signal handler

---
+++ signal/signal.go
@@ -0,0 +1,32 @@
+package signal
+
+import (
+	"os"
+	"os/signal"
+)
+
+func RegisterOSSignalHandler(f func(), signals ...os.Signal) {
+	if len(signals) == 0 {
+		return
+	}
+
+	go func() {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, signals...)
+		<-ch
+
+		f()
+	}()
+}
+
+func WaitOSSignalHandler(f func(), signals ...os.Signal) {
+	if len(signals) == 0 {
+		return
+	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, signals...)
+	<-ch
+
+	f()
+}
